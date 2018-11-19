18410e7fa3174bdbf1146bcbd8aca71fd8b744d1
shinylei <walei@umich.edu>
2018-11-19 14:45:11 -0500

    add burger component and state in burgerBuilder to add ingredients dynamically in burger

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classes from './burgerIngredient.css';
+import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
     render() {
@@ -40,4 +40,4 @@ class BurgerIngredient extends Component {
 BurgerIngredient.PropTypes = {
     type: PropTypes.string.isRequired
 }
-export default burgerIngredient;
+export default BurgerIngredient;
