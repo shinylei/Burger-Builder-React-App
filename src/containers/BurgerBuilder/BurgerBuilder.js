c23f111b7bdeee99d23fba6eb8e64dd2adf37385
shinylei <walei@umich.edu>
2018-11-20 02:40:58 -0500

    add custom button to continue or cancel button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -69,6 +69,10 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     }
 
+    purchaseContinueHandler = () => {
+        alert('continues');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -80,7 +84,10 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary
+                        ingredients={this.state.ingredients} 
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
                 < Burger ingredients={this.state.ingredients} />
                 < BuildControls
